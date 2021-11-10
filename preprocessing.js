393d08bedaffa60ba136c1cc12921929e52a4fa5
smallst <tongsmallst@gmail.com>
2021-11-10 14:28:18 +0800

    preprocessing

--- preprocessing.js
+++ preprocessing.js
@@ -1,4 +1,160 @@
+const fs = require('fs')
+const {hashCode} = require('./hashtbl')
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function(depth = 1) {
+      return this.reduce(function (flat, toFlatten) {
+        return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+      }, []);
+    }
+});
 let get_language_info = (language_file) => {
-  let path_to_language_file = `/language_files/${language_file}`
-  let json = 
+  let path = `./language_files/${language_file}`
+  let rawdata = fs.readFileSync(path);
+  let json = JSON.parse(rawdata);
+  return {
+    keywords: json.keywords,
+    special_chars: json['special characters'],
+    comment_info: {
+      single_comment: json.comment,
+      multi_comment_start: json['comment-start'],
+      multi_comment_end: json['comment-end'],
+      nest: json['comments-nest'],
+      strings: json.strings
+    }
+  }
+}
+
+let rem_white_space = (code_string) => {
+  return code_string.split(/\s+/).filter(s => s != '')
+}
+
+let split_and_keep_on_spec_chars = (spec_chars, str) => {
+  let sp = spec_chars.join('').replace(/\[/g, '\\[').replace(/\]/g, '\\]')
+  return str.split(new RegExp(`([${sp}])`)).filter((s) => s != "")
+}
+
+let split_on_str = (str_to_split_on, acc_str_arr, str_to_split) => {
+  str_to_split_on = str_to_split_on.replace(/\(/g, '\\\(').replace(/\)/g, '\\\)').replace(/\*/g, '\\*')
+  return acc_str_arr.concat(str_to_split.split(new RegExp(`(${str_to_split_on})`)))
+}
+
+let remove_strings = (code_str) => {
+  return code_str.replace(/""|''/g, '').replace(/"[^"]*"/g, 's').replace(/'[^']*'/g, 's')
+}
+
+let remove_comments = (comment_start, comment_end, comments_nest, code_str) => {
+  let split_on_comments_arr = split_on_str(comment_start, [], code_str);
+  let temp =split_on_comments_arr.map((s)=> {
+    return split_on_str(comment_end, [], s)
+  })
+  split_on_comments_arr = temp.flat()
+  let acc_arr = [], nesting = 0
+  for(let str of split_on_comments_arr) {
+    if(str == comment_start) {
+      acc_arr.push(' ');
+      if(comments_nest) {
+        nesting += 1;
+      }
+      else {
+        nesting = 1;
+      }
+    } else if(str == comment_end) {
+      acc_arr.push(' ')
+      if(comments_nest) {
+        nesting -= 1;
+      }
+      else {
+        nesting = 0;
+      }
+    } else {
+      if(nesting == 0) {
+        acc_arr.push(str)
+        acc_arr.push(' ')
+      }
+    }
+  }
+  return acc_arr;
+}
+
+let replace_generics = (keywords, spec_chars, str_arr) => {
+  return str_arr.map((str) => {
+    if(keywords.indexOf(str) != -1 || (str.length == 1 && spec_chars.indexOf(str) != -1)) {
+      return str
+    }
+    else {
+      if(isNaN(+str)) {
+        return 'v'
+      }
+      else {
+        return str
+      }
+    }
+  })
+}
+
+let remove_noise = (comment_info, code_string, keywords, spec_chars, is_txt) => {
+  if(is_txt) return code_string
+  let rm_one_line_comment = (s) => {
+    if(comment_info.single_comment == '') return s;
+    return remove_comments(comment_info.single_comment, "\n", false, s).join('')
+  }
+  let rm_mult_line_comment = (s) => {
+    if(comment_info.multi_comment_start == "") return s;
+    return remove_comments(comment_info.multi_comment_start, comment_info.multi_comment_end, comment_info.nest, s).join('')
+  }
+  let rm_strings = (s) => {
+    if(comment_info.strings) return remove_strings(s)
+    return s
+  }
+  let temp = (rm_one_line_comment(rm_mult_line_comment(rm_strings(code_string))))
+  let temp2 = (split_and_keep_on_spec_chars(spec_chars, temp))
+  let temp3 = temp2.map((t) => {
+    return rem_white_space(t)
+  }).flat()
+  let temp4 = replace_generics(keywords, spec_chars, temp3)
+  return temp4.join('')
+}
+
+let k_grams = (s, k) => {
+  let acc = []
+  for(let i = 0; i < s.length + 1 - k; i++){
+    acc.push(s.substr(i, k))
+  }
+  return acc;
+}
+
+let determine_language_file = (f) => {
+  if(f.endsWith('txt')) return "txt_info.json"
+  if(f.endsWith('c')) return "c_info.json"
+  if(f.endsWith('java')) return "java_info.json"
+  if(f.endsWith('py')) return "python_info.json"
+  console.err('This file format is not supported yet')
+}
+
+let get_ngrams = (f, n) => {
+  let language_file = determine_language_file(f)
+  let language_info = get_language_info(language_file)
+  let keywords = language_info.keywords
+  let spec_chars = language_info.spec_chars
+  let f_string = fs.readFileSync(f);
+  let is_txt = f.endsWith('txt')
+  let com_info = language_info.comment_info
+  let noise_removed_str = remove_noise(com_info, f_string, keywords, spec_chars, is_txt)
+  return k_grams(noise_removed_str, n)
+}
+
+let hash_file = (f) => {
+  let n_grams = get_ngrams(f, 35)
+  return n_grams.map((g) => hashCode(g))
+}
+
+let get_file_positions = (dir, dir_name, filename, positions) => {
+  
+}
+
+module.exports = {
+  get_ngrams,
+  k_grams,
+  remove_noise,
+  get_language_info
 }
---
+++ test_preprocessing.js
@@ -0,0 +1,292 @@
+const {get_ngrams,remove_noise, k_grams, get_language_info} = require('./preprocessing')
+let ocaml_keywords =
+  ["and", "as", "assert", "asr", "begin", "class", "constraint",
+   "do", "done", "downto", "else", "end", "exception", "external",
+   "false", "for", "fun", "function", "functor", "if", "in", "include",
+   "inherit", "initializer", "land", "lazy", "let", "lor", "lsl", "lsr",
+   "lxor", "match", "method", "mod", "module", "mutable", "new", "nonrec",
+   "object", "of", "open", "or", "private", "rec", "sig", "struct", "then",
+   "to", "true", "try", "type", "val", "virtual", "when", "while", "with",
+   "Arg", "Array", "ArrayLabels", "Buffer", "Bytes", "BytesLabels", "Callback",
+   "Char", "Complex", "Digest", "Ephemeron", "Filename", "Format", "Gc",
+   "Genlex", "Hashtbl", "Int32", "Int64", "Lazy", "Lexing", "List",
+   "ListLabels", "Map", "Marshal", "MoreLabels", "Nativeint", "Oo", "Parsing",
+   "Printexc", "Printf", "Queue", "Random", "Scanf", "Set", "Sort",
+   "Spacetime", "Stack", "StdLabels", "Stream", "String", "StringLabels",
+   "Sys", "Uchar", "Weak", "failwith"]
+
+let ocaml_spec_chars =
+  ['!', '$', '%', '&', '*', '+', '-', '.', '/', ':', ';', '<',
+   '=', '>', '?', '@', '^', '|', '~', '#', '"', '(', ')', ',',
+   '[', ']', '{', '}']
+
+let ocaml_comments_info = 
+  {
+    single_comment : "",
+    multi_comment_start : "(*",
+    multi_comment_end : "*)",
+    nest: true,
+    strings: true,
+  }
+
+let java_keywords =
+  ["abstract", "continue", "for", "new", "switch", "assert", "default",
+   "package", "synchronized", "boolean", "do", "if", "private", "this", "break",
+   "double", "implements", "protected", "throw", "byte", "else", "import",
+   "public", "throws", "case", "enum", "instanceof", "return", "transient",
+   "catch", "extends", "int", "short", "try", "char", "final", "interface",
+   "static", "void", "class", "finally", "long", "strictfp", "volatile",
+   "float", "native", "super", "while", "Boolean", "Byte", "Character", "Class",
+   "ClassLoader", "ClassValue", "Compiler", "Double", "Enum", "Float",
+   "InheritableThreadLocal", "Integer ", "Long", "Math", "Number", "Object",
+   "Package", "Process", "ProcessBuilder", "Runtime", "RuntimePermission",
+   "SecurityManager", "Short", "StackTraceElement", "StrictMath", "String",
+   "StringBuffer", "StringBuilder", "System", "Thread", "ThreadGroup",
+   "ThreadLocal", "Throwable", "Void", "AbstractCollection", "AbstractList",
+   "AbstractMap", "AbstractQueue", "AbstractSequentialList", "AbstractSet",
+   "ArrayDeque", "ArrayList", "Arrays", "BitSet", "Calendar", "Collections",
+   "Currency", "Date", "Dictionary", "EnumMap", "EnumSet", "EventListenerProxy",
+   "EventObject", "FormattableFlags", "Formatter", "GregorianCalendar",
+   "HashMap","HashSet", "Hashtable", "IdentityHashMap", "LinkedHashMap",
+   "LinkedHashSet", "LinkedList", "ListResourceBundle", "Locale", "Objects",
+   "Observable", "PriorityQueue", "Properties", "PropertyPermission",
+   "PropertyResourceBundle", "Random", "ResourceBundle",
+   "ResourceBundle.Control", "Scanner", "ServiceLoader", "SimpleTimeZone",
+   "Stack", "StringTokenizer", "Timer", "TimerTask", "TimeZone", "TreeMap",
+   "TreeSet", "UUID", "Vector", "WeakHashMap"]
+
+let java_spec_chars =
+  ['!', '$', '%', '&', '*', '+', '-', '.', '/', ':', ';',
+   '<', '=', '>', '?', '^', '|',  '\"', '\'', '(', ')', ',', '[', ']',
+   '{', '}', '~', '@']
+
+let java_comments_info = 
+  {
+    single_comment : "//",
+    multi_comment_start : "/*",
+    multi_comment_end : "*/",
+    nest: false,
+    strings: true,
+  }
+
+let test_fun_str =
+  `(* Hello World this is a comment *)
+   (* (* This is a nested comment *) *)
+   (* This is a multi line comment
+      This is a multi line comment *)
+   (* This is a multi line (* nested comment
+      Wowie this is *) quite the comment *)
+  let split_and_keep_on_spec_chars spec_chars str =
+  let char_array = str_to_chr_arr str in
+  (List.fold_left
+    (fun acc_arr chr ->
+       let str_of_chr = String.make 1 chr in
+       if List.mem chr spec_chars then
+         List.cons "" (List.cons str_of_chr acc_arr)
+       else
+         (* Hello I am yet another comment *)
+         match acc_arr with
+         | h::t -> (String.concat "" [h,str_of_chr])::t
+         | [] -> failwith "Array should never be empty"
+    )
+    [""]
+    char_array) |> List.filter (fun str -> str <> "") |> List.rev`
+
+let expected_res_str =
+    ["letvvv=letv=vvin(List.v(funvv->letv=String.v1vinifList.vvvthenList.v",
+     "(List.vvv)elsematchvwith|v::v->(String.v[v,v])::v|[]->failwithv",
+     ")[]v)|>List.v(funv->v<>)|>List.v"].join('')
+
+let test_fun_str3 = "wow(*wow wow wow*)wow"
+
+let expected_res_str3 = "vv"
+
+let test_fun_str4 = "wow (* wow wow wow"
+
+let expected_res_str4 = "v"
+
+let test_fun_str5 =
+  `/**
+     * A DumbAI is a Controller that always chooses the blank space with the
+     * smallest column number from the row with the smallest row number.
+     */
+    public class DumbAI extends Controller`
+
+let expected_res_str5 = "publicclassvextendsv"
+
+let test_fun_str6 =
+  `// Note: Calling delay here will make the CLUI work a little more
+    Hello World
+    I am the World
+    hahaha`
+
+let expected_res_str6 = "vvvvvvv"
+
+let test_fun_str7 =
+  `package controller,
+
+import java.util.ArrayList,
+import java.util.List,
+
+import model.Board,
+import model.Game,
+import model.Location,
+import model.NotImplementedException,
+import model.Player,
+
+/**
+ * A DumbAI is a Controller that always chooses the blank space with the
+ * smallest column number from the row with the smallest row number.
+ */
+public class DumbAI extends Controller {
+
+  public DumbAI(Player me) {
+    super(me),
+    // TODO Auto-generated constructor stub
+    //throw new NotImplementedException(),
+  }
+
+  protected @Override Location nextMove(Game g) {
+    // Note: Calling delay here will make the CLUI work a little more
+    // nicely when competing different AIs against each other.
+
+    // TODO Auto-generated method stub
+    //throw new NotImplementedException(),
+
+    Board b = g.getBoard(),
+    // find available moves
+    for (int row = 0,row<Board.NUM_ROWS,row++) {
+      for(int col = 0,col<Board.NUM_COLS,col++) {
+        Location loc = new Location(row,col),
+        if (b.get(loc) == null) {
+          delay(),
+          return loc,
+        }
+
+      }
+    }
+    // wait a bit
+    delay(),
+
+    return null,
+  }
+}`
+
+let ocaml_info = get_language_info("ocaml_info.json")
+let java_info = get_language_info("java_info.json")
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
+
+let assert_equal = (a, b) =>{
+  let eq = JSON.stringify(a) == JSON.stringify(b);
+  if(Array.isArray(a)) {
+    eq = arrayEquals(a, b)
+  }
+  if (!eq) {
+    console.log('not match!')
+    console.log(JSON.stringify(a))
+    console.log(JSON.stringify(b))
+  }
+  else {
+    return 'success'
+  }
+}
+let tests = {
+  "k_grams_2" : () => assert_equal(k_grams("test", 3), ["tes", "est"]),
+  "k_grams_1" : () => assert_equal(k_grams("Hello World", 5),
+                      ["Hello", "ello ", "llo W", "lo Wo", "o Wor", " Worl", "World"]),
+  "ocaml_keywords" : () => assert_equal(
+    ocaml_info.keywords,
+                           ocaml_keywords),
+  "ocaml_spec_chars" : () => assert_equal(
+                             ocaml_info.special_chars, ocaml_spec_chars),
+  "ocaml_comments_info" : () => assert_equal(
+    ocaml_info.comment_info,
+                                ocaml_comments_info),
+
+  "java_keywords" : () => assert_equal(java_info.keywords, java_keywords),
+  "java_spec_chars" : () => assert_equal(java_info.special_chars, java_spec_chars),
+  "java_comments_info" : () => assert_equal(
+    java_info.comment_info,
+                               java_comments_info),
+
+  "remove_noise" :
+  () => assert_equal(
+    remove_noise(
+      ocaml_comments_info,
+      test_fun_str,
+      ocaml_keywords, ocaml_spec_chars,
+      false),
+      expected_res_str),
+
+  "remove_noise_3" :
+  () => assert_equal
+  (remove_noise(
+    ocaml_comments_info,
+    test_fun_str3,
+    ocaml_keywords, ocaml_spec_chars,
+    false),
+      expected_res_str3),
+
+  "remove_noise_4" :
+  () => assert_equal
+  (remove_noise(
+    ocaml_comments_info,
+    test_fun_str4,
+    ocaml_keywords, ocaml_spec_chars,
+    false),
+      expected_res_str4),
+
+  "remove_noise_5" :
+  () => assert_equal
+  (remove_noise(
+    java_comments_info,
+    test_fun_str5,
+    java_keywords, java_spec_chars,
+    false),
+      expected_res_str5),
+
+  "remove_noise_6" :
+  () => assert_equal
+  (remove_noise(
+    java_comments_info,
+    test_fun_str6,
+    java_keywords, java_spec_chars,
+    false),
+      expected_res_str6),
+
+  "remove_noise_7" :
+  () => assert_equal
+  (remove_noise(
+    java_comments_info,
+    "abc<=def",
+    java_keywords, java_spec_chars,
+    false),
+      "v<=v"),
+
+  "remove_noise_8" : () => assert_equal
+  (remove_noise(
+    ocaml_comments_info,
+    "let x = \"(*\" in let y = 5 in \"*)\"",
+    ocaml_keywords,ocaml_spec_chars,
+    false),
+      "letv=vinletv=5inv"),
+
+  "remove_noise_9" :
+  () => assert_equal
+  (remove_noise(
+    ocaml_comments_info,
+    "let x = 'y'",
+    ocaml_keywords,ocaml_spec_chars,
+    false),
+      "letv=v"),
+}
+
+for(const key in tests) {
+  // if(key == 'remove_noise')
+  console.log(key, "::", tests[key]())
+}
